ff219a2acb29aef9ca09e5868409056b084fe827
weiye <claviering@gmail.com>
2016-02-05 17:22:43 +0800

    add function of bubble sort

--- sort.cpp
+++ sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int StraightInsertionSort(int array[],int length);
+int bubbleSort(int array[],int length);
 int main(){
     int length;
     cin>>length;
@@ -8,7 +9,13 @@ int main(){
     for(int i=0;i<length;i++){
         cin>>array[i];
     }
-    StraightInsertionSort(array,length);
+    cout<<"old"<<endl;
+    for(int i=0;i<length;i++)   cout<<array[i]<<" ";
+    cout<<endl;
+    bubbleSort(array,length);
+    cout<<"new"<<endl;
+    for(int i=0;i<length;i++)   cout<<array[i]<<" ";
+    cout<<endl;
     return 0;
 }
 int StraightInsertionSort(int array[],int length){
@@ -18,9 +25,17 @@ int StraightInsertionSort(int array[],int length){
         for(j=i-1;j>=0 && temp<array[j];j--)    array[j+1]=array[j];
         array[j+1]=temp;
     }
-    for(int i=0;i<length;i++)   cout<<array[i];
-    cout<<endl;
     return 0;
 }
-
-
+int bubbleSort(int array[],int length){
+    while(--length){
+        for(int cur=1;cur<length+1;cur++){
+            if(array[cur-1] > array[cur]){
+                array[cur-1] ^= array[cur];
+                array[cur]   ^= array[cur-1];
+                array[cur-1] ^= array[cur];
+            }
+        }
+    }
+    return 0;
+}
