f1305d40cd0cc64388068f97398d300d0c3241fe
weiye <claviering@gmail.com>
2016-02-23 23:10:21 +0800

    add Cut The Sticks

---
+++ cut_the_sticks.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0; arr_i < n; arr_i++)
+    {
+        cin >> arr[arr_i];
+        if(arr[arr_i] <= 0)  n--;
+    }
+    cout << n << endl;
+    sort (arr.begin(),arr.end());
+    reverse (arr.begin(),arr.end());
+    int cut = arr[n-1];
+    while(n)
+    {
+        for(int arr_i = 0; arr_i < n; arr_i++)
+        {
+            arr[arr_i] -= cut;
+            if(arr[arr_i] <= 0) 
+            {
+                n = arr_i;
+                break;
+            }
+        }
+        if(n > 0)
+        cout << n << endl;
+    }
+    return 0;
+}
+
+
