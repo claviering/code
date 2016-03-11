1aa171be02cf4aa75af0922b8c528708ccda612f
weiye <claviering@gmail.com>
2016-03-11 12:57:59 +0800

    1067

--- ZOJ/1067.c
+++ ZOJ/1067.c
@@ -2,8 +2,6 @@
 int main()
 {
     int arr[48];
-    int r,g,b;
-    int minI[3] = {0};
     int i;
     for (i = 0; i < 48; i++)
     {
@@ -11,10 +9,12 @@ int main()
         scanf ("%d",&temp);
         arr[i] = temp;
     }
-    while (scanf ("%d %d %d",&r,&g,&b) != EOF && r >= 0)
+    int r,g,b;
+    while (scanf ("%d %d %d",&r,&g,&b) != EOF && r >= 0 && g >= 0 && b >= 0)
     {
+        int minI[3] = {0};
+        int dMax = 10000000;
         int i;
-        int dMax = 100000;
         for (i = 0; i <= 45; i += 3)
         {
            int d = (r - arr[i])*(r - arr[i]) + (g - arr[i+1])*(g - arr[i+1]) + (b - arr[i+2])*(b - arr[i+2]);
@@ -27,7 +27,6 @@ int main()
            }
         }
         printf ("(%d,%d,%d) maps to (%d,%d,%d)\n",r,g,b,minI[0],minI[1],minI[2]);
-    int minI[3] = {0};
     }
     return 0;
 }
