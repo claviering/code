bf8d7dbe9a6ba27ea4ab230dadcb1b64de7f977a
weiye <claviering@gmail.com>
2016-04-03 12:38:22 +0800

    B

---
+++ SCNUACM2016/day-two/B.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long n;
+    cin >> n;
+    const int div = 2520;
+    long long num = n / div;
+    cout << num << endl;
+    return 0;
+}
+
