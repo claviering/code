2a36df7b43adcd4e5177f82c4d6d278d3b7821fa
weiye <claviering@gmail.com>
2016-04-12 23:37:29 +0800

    hello

--- hello.cpp
+++ hello.cpp
@@ -14,6 +14,8 @@ int main()
     cout << "hello world" << endl; 
     cout << "hello world" << endl; 
     cout << "hello world" << endl; 
+    cout << "hello world" << endl; 
+    cout << "hello world" << endl; 
     return 0;
 }
 
