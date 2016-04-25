2821bbd6320df26c24e1bebc21c428a4a87d2c70
weiye <claviering@gmail.com>
2016-04-25 23:57:33 +0800

    build my tree

---
+++ fun/mytree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Note
+{
+    private:
+        Note *_l;
+        Note *_r;
+        int _date;
+};
+
+class Tree
+{
+    private:
+        tree *_root;
+};
+
+int main()
+{
+     
+    return 0;
+}
+
