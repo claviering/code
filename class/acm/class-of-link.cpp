244887fee68913e5b2facebc8a54132ca87b3ed9
weiye <claviering@gmail.com>
2016-04-29 23:27:15 +0800

    make a form to accomplish tomorrow

--- class/acm/class-of-link.cpp
+++ class/acm/class-of-link.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Node
 {
-    private:
+    public:
         Node *_next;
         int _data;
 };
@@ -13,7 +13,7 @@ class Link
     private:
         Node *head;
     public:
-        Node *creat(Node *h);
+        Node *creat(int data);
         void display(Node *h);
         void bubbleSort(Node *h);
         void inverseLink(Node *h);
@@ -27,14 +27,26 @@ Link :: Link()
     head = NULL;
 }
 
-Node Node :: *creat(Node *h)
+Node Link:: *creat(int data)
 {
+    Node *head;
     Node *p = new Node; 
+    p -> _data = data;
+    p -> _next = head;
+    head = p;
+}
+
+void display(Node *h)
+{
+    while (h != NULL)
+    {
+        cout << h -> _data << endl;
+        h = h -> _next;
+    }
 }
 
 int main()
 {
-     
     return 0;
 }
 
