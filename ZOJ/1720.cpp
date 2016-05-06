6833cb586ad7eebfd6a654876a800a97fad10c00
claviering <claviering@gmail.com>
2016-05-06 13:14:42 +0800

    great

--- ZOJ/1720.cpp
+++ ZOJ/1720.cpp
@@ -2,76 +2,87 @@
 #include <cmath>
 using namespace std;
 
-const char space = ' ';
-const char nega = '-';
-const char zore = '0';
+const char NEGA = '-';
+const char POSI = '+';
+const char SPAC = ' ';
+const string POLY[8] = {"x^8","x^7","x^6","x^5","x^4","x^3","x^2","x^1"};
 
 int main()
 {
-    string input;
-    while (getline(cin,input))
+    int input[9];
+    while (cin >> input[0] >> input[1] >> input[2] >> input[3] >> input[4] >> input[5] >> input[6] >> input[7] >> input[8])
     {
-        string polynomial[8] = {"x^8","x^7","x^6","x^5","x^4","x^3","x^2","x^1"};
-        int coefficients[9];
-        int inputSize = input.size();
-
-        int i = 0;
-        while (i < inputSize)
+        string output;
+        bool firstCout = 1;
+        for (int i = 0; i < 8; i++)
         {
-            int coeIndex = 0;
-            bool  isNegated = 0;
-            int coe = 0;
-
-            while (input[i] != space)
+            int tmp = input[i];
+            if (tmp < 0)
             {
-                if (input[i] == nega)
-                {
-                    isNegated = 1;
-                    ++i;
-                }
-                int tmp = input[i++] - zore;
-                coe = coe * 10 + tmp;
+               if (firstCout) 
+               {
+                    cout << NEGA << SPAC << abs (input[i]) << POLY[i];
+                    firstCout = 0;
+               }
+               else
+               {
+                    cout << SPAC << NEGA << SPAC << abs (input[i]) << POLY[i];
+               }
             }
-
-            if (isNegated)
-                coefficients[coeIndex] = -coe;
-            else
-                coefficients[coeIndex] = coe;
-            ++i;
-            ++coeIndex;
-        }
-        
-        int k = 0;
-        while (k < 8)
-        {
-            if (coefficients[k] < 0)
-            {
-                cout << "- " << abs(coefficients[k]); 
-                cout << polynomial[k++];
-            }
-            else if (coefficients[k] == 0)
+            else if (tmp == 0)
             {
                 continue;
             }
-            else if (coefficients[k] == 1)
+            else if (tmp == 1)
             {
-                cout << polynomial[k++];
+               if (firstCout) 
+               {
+                    cout << POLY[i];    
+                    firstCout = 0;
+               }
+               else
+               {
+                    cout << SPAC << POSI << SPAC << POLY[i];    
+               }
             }
-            else
+            else 
             {
-                cout << "+ " << abs(coefficients[k]); 
-                cout << polynomial[k++];
+               if (firstCout) 
+               {
+                    cout << input[i] << POLY[i];
+                    firstCout = 0;
+               }
+               else
+               {
+                    cout << SPAC << POSI << SPAC << input[i] << POLY[i];
+               }
+
             }
         }
-
-        if (coefficients[8])
-            cout << "+ "<< coefficients[8];
+        if (firstCout)
+        {
+            if (input[8] < 0)
+                cout << NEGA << abs(input[8]) << endl;
+            else if (input[8] == 0)
+                cout << endl;
+            else if (input[8] == 1)
+                cout << input[8] << endl;
+            else 
+                cout << input[8] << endl;
+        }
         else
-            cout << "- " << abs(coefficients[8]); 
-        cout << endl;
-
+        {
+            if (input[8] < 0)
+                cout << SPAC << NEGA  << SPAC << abs(input[8]) << endl;
+            else if (input[8] == 0)
+                cout << endl;
+            else if (input[8] == 1)
+                cout << SPAC << POSI << SPAC << input[8] << endl;
+            else 
+                cout << SPAC << POSI << SPAC << input[8] << endl;
+            
+        }
     }
-
     
     return 0;
 }
