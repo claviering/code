54ef97d5e397bf77d2377589ff4f4549875ddfca
weiye <claviering@gmail.com>
2016-05-07 21:37:57 +0800

    write a little

---
+++ ZOJ/2724.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+const string GET = "GET";
+const string PUT = "PUT";
+
+int main()
+{
+    queue<string> msg;
+    string input;
+    while (cin >> input) 
+    {
+        if (input == GET)
+        {
+            if ()
+        }
+    }
+    return 0;
+}
+
