8611c187cf8f4f0ac1275a7f615c4534dc915403
claviering <claviering@gmail.com>
2016-05-08 23:55:19 +0800

    unfinish

--- ZOJ/2724.cpp
+++ ZOJ/2724.cpp
@@ -4,16 +4,39 @@ using namespace std;
 
 const string GET = "GET";
 const string PUT = "PUT";
+const string ENMTYQUE = "ENMTY QUEUE!";
+
+class Message
+{
+    public:
+        string inputMsg;
+        int para;
+        int pri;
+};
 
 int main()
 {
-    queue<string> msg;
+    priority_queue<string> msg;
     string input;
     while (cin >> input) 
     {
         if (input == GET)
         {
-            if ()
+            if (msg.empty())
+            {
+                cout << ENMTYQUE << endl;
+            }
+            else
+            {
+                msg.top();
+                msg.pop();
+            }
+        }
+        else
+        {
+            Message mes;
+            cin >> mes.inputMsg >> mes.para >> mes.pri;
+
         }
     }
     return 0;
