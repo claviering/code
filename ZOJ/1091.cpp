cc29639d092139a201afe700e5c2fe5f76d97b5d
claviering <claviering@gmail.com>
2016-05-14 23:40:39 +0800

    fuck 1091

---
+++ ZOJ/1091.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int x[] = {1,1,2,2,-1,-1,-2,-2};
+int y[] = {2,-2,1,-1,2,-2,1,-1};
+
+int main()
+{
+    char a[10];
+    char b[10];
+     
+    return 0;
+}
+
