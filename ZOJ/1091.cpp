bfd3f6365903074e0f47c52ecabc55c25764b6d3
claviering <claviering@gmail.com>
2016-05-18 23:33:16 +0800

    1091

--- ZOJ/1091.cpp
+++ ZOJ/1091.cpp
@@ -5,19 +5,20 @@ using namespace std;
 const string display0 = "To get from ";
 const string display1 = " to ";
 const string display2 = " takes ";
-const string display3 = " knight moves";
+const string display3 = " knight moves.";
+
 int xMove[] = {1,1,2,2,-1,-1,-2,-2};
 int yMove[] = {2,-2,1,-1,2,-2,1,-1};
 
 int move[8][8];
 
-void bfs(int x,int y,int step,int xExit,int yExit)
+void bfs(int x,int y,int step)
 {
-    if (x<0 || y<0 || x>8 || y>8 || move[x][y] <= step || x == xExit || y == yExit)
+    if (x<0 || y<0 || x>7 || y>7 || move[x][y] < step)
         return;
     move[x][y] = step++;
     for (int i = 0; i < 8; i++)
-        bfs(x + xMove[i],y + yMove[i],step,xExit,yExit);
+        bfs(x + xMove[i],y + yMove[i],step);
 
 }
 
@@ -32,11 +33,10 @@ int main()
         int xExit = b[0] - 'a';
         int yExit = b[1] - '1';
         memset (move,10,sizeof (move));
-        bfs(x,y,0,xExit,yExit);
+        bfs(x,y,0);
         int knightMoves = move[xExit][yExit];
         cout << display0 << a << display1 << b << display2 << knightMoves << display3 << endl;
     }
      
     return 0;
 }
-
