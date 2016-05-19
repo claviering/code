c5d5d77de1f3a8e40fc4675a5438bcadf972ef68
claviering <claviering@gmail.com>
2016-05-19 23:24:27 +0800

    day two 1204

--- ZOJ/1204.cpp
+++ ZOJ/1204.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        while (n--)
+        {
+            int integers[n];
+            for (int j = 0; j < n; j++)
+            {
+                cin >> integers[j];
+            }
+        }
+    }
     return 0;
 }
 
