f24e3da9acdf5daf1845a21def1db4734cd01a6a
claviering <claviering@gmail.com>
2016-05-26 23:02:28 +0800

    start 2734

---
+++ ZOJ/2734.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int m;
+    while (cin >> n >> m)
+    {
+        while (m--)
+        {
+
+        }
+    }
+    return 0;
+}
+
